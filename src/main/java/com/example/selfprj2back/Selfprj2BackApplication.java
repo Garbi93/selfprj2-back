817ff561ba619cdb5a5398301a05e157fa967359
Garbi93 <iseung2093@gmail.com>
2024-06-19 14:07:25 +0900

    6/19 self project backend setting

---
+++ src/main/java/com/example/selfprj2back/Selfprj2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfprj2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selfprj2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Selfprj2BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selfprj2back/Selfprj2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfprj2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Selfprj2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
